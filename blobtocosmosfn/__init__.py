f698146daee557947c6aa2833118736b31cf6ecd
Sumit Asok <sumitasok@gmail.com>
2020-03-26 17:16:15 +0530

    blobStorageApp

--- blobtocosmosfn/__init__.py
+++ blobtocosmosfn/__init__.py
@@ -1,10 +1,11 @@
 import logging
 
 import azure.functions as func
+from azure.storage.blob import BlockBlobService
 import json
 
 
-def main(myblob: func.InputStream, doc: func.Out[func.Document]):
+def main(myblob: func.InputStream, doc: func.Out[func.Document], myOutblob: func.Out[func.InputStream]):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes\n"
@@ -18,3 +19,11 @@ def main(myblob: func.InputStream, doc: func.Out[func.Document]):
         }
     }
     doc.set(func.Document.from_json(json.dumps(outdata)))
+
+    # block_blob_service = BlockBlobService(
+    #     account_name='storageaccountazure8ed8',
+    #     account_key='jz9uITvczqYowa9oKJnL3h/4pISy+lxwilNNFRkC5GWdbzWPKMp8ClRjv4iPM5r7xCHvnYAF3HxonnD4ROf0MA=='
+    # )
+
+    # block_blob_service.get_blob_to_path('samples-workitems', '0002.pdf', '/tmp/file.pdf')
+    # block_blob_service.create_blob_from_path('raster-derived', '0002.pdf', '/tmp/file.pdf')
